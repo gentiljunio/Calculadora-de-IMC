856914dfd71435a74c10f6df95c7bd0d604428d9
Gentil Junio <88815184+gentiljunio@users.noreply.github.com>
2026-02-13 11:18:02 -0300

    Refactor calcularIMC and classificacao functions

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 function calcularIMC() {
-  const peso = parseFloat(document.getElementById("peso").value); //Float = decimal Ex: 1.74
+  const peso = parseFloat(document.getElementById("peso").value);
   const altura = parseFloat(document.getElementById("altura").value);
 
-  const imc = (peso / (altura * altura)).toFixed(1); //toFixed() é um método do JavaScript usado para controlar quantas casas decimais um número vai mostrar.
+  const imc = (peso / (altura * altura)).toFixed(1);
 
   document.getElementById("p").innerHTML =
     "Seu IMC é " + imc + " - " + classificacao(imc);
 }
 
 function classificacao(imc) {
-  if (imc < 18.5) return "Magreza";
+  if (imc < 18.5)
+    return "Magreza";
 
-  if (imc >= 18.5 && imc <= 24.9) return "Normal";
+  if (imc >= 18.5 && imc <= 24.9)
+    return "Normal";
 
-  if (imc >= 25 && imc <= 29.9) return "Sobrepeso I";
+  if (imc >= 25 && imc <= 29.9)
+    return "Sobrepeso I";
 
-  if (imc >= 30 && imc <= 39.9) return "Obesidade nível II";
+  if (imc >= 30 && imc <= 39.9)
+    return "Obesidade nível II";
 
-  if (imc >= 40) return "Obesidade nível III";
+  if (imc >= 40)
+    return "Obesidade nível III";
 }
+
